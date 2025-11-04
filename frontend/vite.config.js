3eec3d7b1f20f7f1f1089f7a70e9298142974f8f
ryanpaul46 <ryanpaul46@gmail.com>
2025-11-04 21:36:36 +0800

    Deployment for testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/DMW-RO1-Website/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     port: 3000,
     proxy: {
