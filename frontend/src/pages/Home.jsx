f6bf93df16bc72273a456f2391a875d591783cf2
ryanpaul46 <ryanpaul46@gmail.com>
2025-11-05 09:15:55 +0800

    updated images link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
           <Carousel.Item>
             <img
               className="d-block w-100 carousel-img"
-              src="/public/images/DMW RO1.jpg"
+              src="/images/DMW RO1.jpg"
               alt="DMW Services"
             />
             <Carousel.Caption>
@@ -36,7 +36,7 @@ const Home = () => {
           <Carousel.Item>
             <img
               className="d-block w-100 carousel-img"
-              src="/public/images/DMW PACD.jpg"
+              src="/images/DMW PACD.jpg"
               alt="OFW Support"
             />
             <Carousel.Caption>
